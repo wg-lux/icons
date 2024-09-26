ca28d43a9f068ed64b0773b97efb761ae956e959
maddonix <tlux14@googlemail.com>
2024-09-26 16:00:34 +0200

    initial publish

--- flake.nix
+++ flake.nix
@@ -1,72 +1,123 @@
 {
-  description = "Flake for the Django based `agl-monitor` service";
+  description = "Flake for the Django-based `agl-anonymizer` service with CUDA support";
+
+  nixConfig = {
+    substituters = [
+        "https://cache.nixos.org"
+        "https://cuda-maintainers.cachix.org"
+      ];
+    trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      ];
+    extra-substituters = "https://cache.nixos.org https://nix-community.cachix.org https://cuda-maintainers.cachix.org";
+    extra-trusted-public-keys = "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E=";
+  };
+
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    
+    # Use a single nixpkgs input to avoid conflicts
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # poetry2nix should follow the same nixpkgs
     poetry2nix.url = "github:nix-community/poetry2nix";
     poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
 
+    cachix = {
+      url = "github:cachix/cachix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  # @inputs is a shorthand for { nixpkgs = inputs.nixpkgs; ... }
-  # This is useful to avoid repeating the same prefix over and over.
-  # "..." is a special attribute that refers to the rest of the attributes.
-  outputs = { nixpkgs, poetry2nix, ... } @ inputs: 
-  let 
+  outputs = { nixpkgs, cachix, ... } @ inputs:
+  let
+    nvidiaCache = cachix.lib.mkCachixCache {
+        inherit (pkgs) lib;
+        name = "nvidia";
+        publicKey = "nvidia.cachix.org-1:dSyZxI8geDCJrwgvBfPH3zHMC+PO6y/BT7O6zLBOv0w=";
+        secretKey = null;  # not needed for pulling from the cache
+      };
+
     system = "x86_64-linux";
     self = inputs.self;
-    version = "0.1.${pkgs.lib.substring 0 8 inputs.self.lastModifiedDate}.${inputs.self.shortRev or "dirty"}";
-    python_version = "311";
-  
-    raw-pkgs = import nixpkgs {
+
+    # Import nixpkgs with desired configuration
+    pkgs = import nixpkgs {
       inherit system;
-      config.allowUnfree = true;
-
-    
+      config = {
+        allowUnfree = true;
+        cudaSupport = true;
+      };
     };
-    # note that we use "inputs.nixpkgs-unstable" to refer to the flake input
-    # We need to do that because in "outputs = { nixpkgs, ... } @ inputs:",
-    # nixpkgs is not passed directly but the "..." overflow attribute is passed to "inputs".
-    pkgs-unstable = inputs.nixpkgs-unstable.legacyPackages.x86_64-linux;
 
-    pkgs = raw-pkgs.extend poetry2nix.overlays.default;
+    pypkgs-build-requirements = {
+      gender-guesser = [ "setuptools" ];
+      conllu = [ "setuptools" ];
+      janome = [ "setuptools" ];
+      pptree = [ "setuptools" ];
+      wikipedia-api = [ "setuptools" ];
+      django-flat-theme = [ "setuptools" ];
+      django-flat-responsive = [ "setuptools" ];
+    };
+
+
+    poetry2nix = inputs.poetry2nix.lib.mkPoetry2Nix { inherit pkgs;};
+
     lib = pkgs.lib;
 
-    poetryApplication = pkgs.poetry2nix.mkPoetryApplication {
+    p2n-overrides = poetry2nix.defaultPoetryOverrides.extend (final: prev:
+      builtins.mapAttrs (package: build-requirements:
+        (builtins.getAttr package prev).overridePythonAttrs (old: {
+          buildInputs = (old.buildInputs or [ ]) ++ (
+            builtins.map (pkg:
+              if builtins.isString pkg then builtins.getAttr pkg prev else pkg
+            ) build-requirements
+          );
+        })
+      ) pypkgs-build-requirements
+      // { ###################### DELETE?!  
+        pytorch = prev.pytorch.override {
+          cudaSupport = true;
+          cudatoolkit = pkgs.cudatoolkit; # Adjust this version if necessary
+        };
+      }
+    );
+
+
+  in {
+
+    packages.x86_64-linux.poetryApp = poetry2nix.mkPoetryApplication {
+      python = pkgs.python311;
       projectDir = ./.;
       src = lib.cleanSource ./.;
-      python = pkgs."python${python_version}Full";
-    };       
-    
-  in
-  {
+      overrides = p2n-overrides;
 
-    # Create Default DevShell
-    # https://ryantm.github.io/nixpkgs/builders/special/mkshell/
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        python311Full
-        poetry
+      preferWheels = true; # required for transformers via p2n
+
+      propagatedBuildInputs =  with pkgs.python311Packages; [
+        pillow
+        potrace
       ];
 
-      packages = [
-        pkgs.python311Full 
-        pkgs.python311Packages.django
-      ];
-
-      inputsFrom = [];
+      buildInputs = with pkgs; [];
     };
 
-    packages.x86_64-linux.default = poetryApplication;
+    
 
-    # Create python App
-    apps.x86_64-linux.default = {
-        type = "app";
-        # replace <script> with the name in the [tool.poetry.scripts] section of your pyproject.toml
-        program = "${poetryApplication}/bin/django-server";
+
+    nixConfig = {
+        binary-caches = [
+          nvidiaCache.binaryCachePublicUrl
+        ];
+        binary-cache-public-keys = [
+          nvidiaCache.publicKey
+        ];
+        # enable cuda support
+        cudaSupport = true;
       };
-  
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      inputsFrom = [ self.packages.x86_64-linux.poetryApp ];
+      packages = [ pkgs.poetry ];
+    };
   };
 }
